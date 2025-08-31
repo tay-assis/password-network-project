9d9bb102dfdda1ec50bf194f1852ea015e5b803f
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2025-08-31 19:39:43 -0300

    estrutura base angular

--- password-network/src/app/app.ts
+++ password-network/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './views/home/home';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  selector: 'home',
+  imports: [],
+  templateUrl: './views/home/home.html',
+  styleUrl: './views/home/home.scss'
 })
 export class App {
   protected readonly title = signal('password-network');
